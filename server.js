769d8f2b38b4bf69d45df8a7e88ea5676854c176
nosson <48989814+nossonCotlar@users.noreply.github.com>
2019-07-28 01:42:39 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.get('/', function(request, response){
+    //response.send('Hello World');
+    var check = verify(request.query.user, request.query.key);
+    
+    console.log(request.query.user + ' ' + request.query.key + ': ' + check);
+    response.send(check);
+    response.end();
+});
+
+app.listen(port, function(){
+console.log('Server ready :)');
+});
+
+function verify(user, key){
+    var users = fs.readFileSync('users.json');
+    var jsonContent = JSON.parse(users);
+    return jsonContent[user].key === key;
+}
