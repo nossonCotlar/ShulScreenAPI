7a89940a1a7c6747eea3aaddcd49de5b2307b782
nosson <48989814+nossonCotlar@users.noreply.github.com>
2019-07-30 16:22:02 -0500

    clean up

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response){
 
     var timeNow = new Date();
     if(check){
-        console.log(timeNow.toUTCString() + '>>' + request.query.user + ' ' + request.query.key + ': licensed' + '\n');
+        console.log(timeNow.toUTCString() + '>>  ' + request.query.user + ' ' + request.query.key + ': licensed' + '\n');
     }
     else{
         console.log(timeNow.toUTCString() + '>>  ' + request.query.user + ' ' + request.query.key + ': unlicensed' + '\n');
@@ -38,10 +38,13 @@ console.log('Server ready :)');
 function verify(user, key){
     var users = fs.readFileSync('users.json');
     var jsonContent = JSON.parse(users);
-    console.log(user + ": " + jsonContent[user]);
+    console.log(user + ": " + JSON.stringify(jsonContent[user]));
     return jsonContent[user].key === key;
 }
 
 function addLicense(user, type, key){
-
+var text = fs.readFileSync('users.json', 'utf8');
+var cut = text.substring(0, text.length - 2);
+var full = cut + '\n\n"' + user + '":{\n"key":"' + key + '",\n"licenseType":"' + type + '",\n"dateAdded":""\n},\n\n}';
+fs.writeFileSync('users.json', full);
 }
