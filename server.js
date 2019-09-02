72f58df655f49b6c21b52e2c5ad875b9b607f61c
nosson <48989814+nossonCotlar@users.noreply.github.com>
2019-09-02 14:05:30 -0500

    Zmanim info from Main Post

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const productVersion = 'alpha.1.5';
+const zmanimURL = 'https://api.myzmanim.com/engine1.json.aspx';
+const zmanimUser = '0010921043';
+const zmanimKey = 'ff71bd8bec18ecc3a6716ca8e6462f52646b8a774d1b292d63d37ee9618cede940d4ae97277af79d';
 
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const express = require('express');
 var https = require('https');
 var parser = require('body-parser');
@@ -24,17 +28,10 @@ app.use(parser.json('application/json'));
 
 app.post('/mainPost', mainPost);
 
-app.get('/verify', verifyWrapper);
-
 app.post('/postadd', addLicenseFromPost);
 
 app.post('/textSubmit', addToPendingSyncs);
 
-app.get('/textPull/:user/:key', sendSubmissionToClient);
-
-app.get('/parsha/:parsha/:day', sendParshaContent)
-
-app.get('/version', versionCheck);
 
 app.listen(port, function () {
     console.log('Listening on ' + port + '...');
@@ -71,13 +68,14 @@ function mainPost(request, response){
         verify: verify(request.body.user, request.body.key), 
         updateAvailable: request.body.version !== productVersion, 
         parsha: parsha, 
+        zmanim: zmanimPost(request.body.postalCode, request.body.dateString), 
         donors: donors, 
         announcements: announcements, 
         memorial: memorial
     };
 
     response.send(JSON.stringify(jsonResponse, null, 2));
-    console.log('sent to:' + request.ip + '\n' + JSON.stringify(jsonResponse, null, 2));
+    //console.log('sent to:' + request.ip + '\n' + JSON.stringify(jsonResponse, null, 2));
 
 }
 
@@ -129,42 +127,6 @@ function addToPendingSyncs(request, response) {
     response.end("Submission Successful: \n" + JSON.stringify(toPush, null, 2));
     console.log("Submission Successful: \n" + JSON.stringify(toPush, null, 2));
 }
-
-function sendSubmissionToClient(request, response) {
-    var objToSend = {available: false};
-    var pendingSyncs = JSON.parse(fs.readFileSync('pendingSyncs.json'));
-    for (var i = 0; i < pendingSyncs.length; i++) {
-        if (pendingSyncs[i] == null) continue;
-        if (pendingSyncs[i].user == request.params.user && pendingSyncs[i].key == request.params.key) {
-            objToSend.available = true;
-            objToSend.selection = pendingSyncs[i].selection;
-            objToSend.text = pendingSyncs[i].text;
-            pendingSyncs.splice(i, 1);
-            fs.writeFileSync('pendingSyncs.json', JSON.stringify(pendingSyncs, null, 2));
-            break;
-        }
-        
-    }
-    response.end(JSON.stringify(objToSend, null, 2));
-    //if(objToSend.available == false) console.log("nothing was available");
-    if(objToSend.available == true) console.log("Sent Sync Content to client and deleted from Database: \n" + 
-    JSON.stringify(objToSend, null, 2));
-}
-
-function verifyWrapper(request, response) {
-    var check = verify(request.query.user, request.query.key); //as of now it accepts requests made as URL queries, will switch to params later
-
-
-    response.send(check);
-    response.end();
-
-    if (check) {
-        console.log(request.ip + ' >>  ' + request.query.user + ' ' + request.query.key + ': licensed' + '\n');
-    }
-    else {
-        console.log(request.ip + ' >>  ' + request.query.user + ' ' + request.query.key + ': unlicensed' + '\n');
-    }
-}
 function verify(user, key) {
     var users = fs.readFileSync('new.json', 'utf8'); //reads file data
     var jsonContent = JSON.parse(users); //make it json
@@ -180,7 +142,6 @@ function verify(user, key) {
     return (jsonSingle.key === key); //return whether the key from request query matches the single object's key
 
 }
-
 function addLicenseFromPost(request, response) {
 
     var objToAdd = request.body;
@@ -205,14 +166,19 @@ function addLicenseFromPost(request, response) {
 
 }
 
-function sendParshaContent(request, response){
-    var parshaPath = 'parsha/' + request.params.parsha + '/' + request.params.day + '.txt';
-    var parshaFile = fs.readFileSync(parshaPath, 'ascii');
-    response.end(parshaFile);
-    //console.log(parshaFile);
-    console.log('Sent parsha content in: ' + parshaPath + ' to: ' + request.ip)
+function zmanimPost(postalCode, dateString){
+    var xhr = new XMLHttpRequest();
+    var params = 'coding=JS&language=en' + 
+    '&locationid=US' + postalCode + 
+    '&inputdate=' + dateString + 
+    '&key=' + zmanimKey + 
+    '&user=' + zmanimUser;
+
+    xhr.open('POST', zmanimURL + '/getDay', false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+
+    return xhr.responseText;
+
 }
 
-function versionCheck(request, response) {
-    response.end(productVersion);
-}
